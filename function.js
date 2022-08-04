ba61a7acd9e2a5d99d599e11a8c46a4b85f8b916
Howard <howardchen1617@gmail.com>
2022-08-04 14:02:57 +0800

    add splice.js

--- function.js
+++ function.js
@@ -108,8 +108,67 @@ function reduce(arr, callback, initialValue) {
 // ===== reduce end =====
 
 
+function splice(arr, start, deleteCount, ...addItems) {
+    Array.isArray(arguments[0]) ? '' : error('第一項 argument 必須是 array 形式');
+    start = isNaN(Number(start)) ? 0 : Number(start); // 將該參數轉為數字型別，若出現 NaN 則賦值為 0
+    start = start < 0 ?  arr.length + start : start; // 如果起始索引是負數，則會從陣列中最後一個元素開始往前改動，這裡重新賦值為正確索引
+    start = start < 0 ?  0 : start ; // 如果起始索引是負數，且超過陣列長度，則重新賦值為 0
+    start = start > arr.length ?  arr.length : start; // 如果起始索引超過陣列長度，則重新賦值為陣列長度
+
+    if (deleteCount == undefined) {
+        let results = []
+        let resultsIndex = 0;
+        for (let index = start; index < arr.length-start; index++) {
+            results[resultsIndex] = arr[index];
+            resultsIndex++;
+        }
+        arr.length = start;
+        return results;        
+    }
+
+    deleteCount = isNaN(Number(deleteCount)) ? 0 : Number(deleteCount); // 將該參數轉為數字型別，若出現 NaN 則賦值為 0
+    deleteCount = deleteCount < 0 ?  0 : deleteCount; // 如果刪除數量是負數，則重新賦值為 0
+    deleteCount = arr.length - start - deleteCount < 0 ?  arr.length - start : deleteCount; // 如果從起始索引算起的刪除數量超過陣列可以被刪除的數量，則重新賦值為可以被刪除的數量
+    
+    let finalLength = arr.length - deleteCount + addItems.length;
+    let items_keep_1 = [];
+    let items_delete = [];
+    let items_keep_2 = [];
+    let finalArr = [];
+    let finalArrIndex = 0;
+
+    for (let index = 0; index < start; index++) {
+        items_keep_1[index] = arr[index]; 
+        finalArr[finalArrIndex] = arr[index];
+        finalArrIndex++;
+    }
+
+    for (const item of addItems) {
+        finalArr[finalArrIndex] = item;
+        finalArrIndex++;
+    }
+    
+    for (let index = 0; index < deleteCount; index++) {
+        items_delete[index] = arr[index + start];
+    }
+
+    for (let index = 0; index < arr.length - start - deleteCount; index++) {
+        items_keep_2[index] = arr[index + start + deleteCount];
+        finalArr[finalArrIndex] = arr[index + start + deleteCount];
+        finalArrIndex++;
+    }
+
+    for (let index = 0; index < finalArr.length; index++) {
+        arr[index] = finalArr[index];
+    }
+
+    return items_delete;
+}
+// ===== splice end =====
+
+
 function error(text) {
     throw new Error(text);
 }
 
-export {filter, concat, find ,findIndex, map, reduce};
+export {filter, concat, find ,findIndex, map, reduce, splice};
--- main.js
+++ main.js
@@ -27,6 +27,42 @@ let obj = {
     'price': 300
 }
 
+let arr_old_Splice = [
+    {
+        'id': 1,
+        'type': 'a',
+        'price': 100
+    },
+    {
+        'id': 2,
+        'type': 'b',
+        'price': 100
+    },
+    {
+        'id': 3,
+        'type': 'a',
+        'price': 200
+    }
+];
+
+let arr_new_Splice = [
+    {
+        'id': 1,
+        'type': 'a',
+        'price': 100
+    },
+    {
+        'id': 2,
+        'type': 'b',
+        'price': 100
+    },
+    {
+        'id': 3,
+        'type': 'a',
+        'price': 200
+    }
+];
+
 
 // console.log(func.filter(arr_1, (item) => { return item.price < 200 }));
 // console.log(arr_1.filter((item) => { return item.price < 200 })); 
@@ -56,5 +92,20 @@ let obj = {
 // console.log(func.reduce(arr_1, (accumulator ,currentValue, currentIndex, array) => { return accumulator + currentValue.price }, 0));
 // console.log(arr_1.reduce((accumulator ,currentValue, currentIndex, array) => { return accumulator + currentValue.price }, 0));
 // ? 原 reduce 陣列方法
+
+console.log('======= 這是實作 splice 方法 ======');
+console.log('原始陣列：', arr_new_Splice);
+func.splice(arr_new_Splice, -1, 0, { id:'測試 id', type:'測試 type', price:'測試 price'}, 0, [1, 2, [321, 99]])
+console.log('改動後陣列：', arr_new_Splice);
+console.log('return ', func.splice(arr_new_Splice, -1, 0, { id:'測試 id', type:'測試 type', price:'測試 price'}, 0, [1, 2, [321, 99]]));
+
+console.log('----------------------------------------------');
+
+console.log('======= 這是原 splice 方法 ======');
+console.log('原始陣列：', arr_old_Splice);
+arr_old_Splice.splice(-1, 0, { id:'測試 id', type:'測試 type', price:'測試 price'}, 0, [1, 2, [321, 99]])
+console.log('改動後陣列：', arr_old_Splice);
+console.log('return ', arr_old_Splice.splice(-1, 0, { id:'測試 id', type:'測試 type', price:'測試 price'}, 0, [1, 2, [321, 99]]));
+// ? 原 splice 陣列方法
 
 
