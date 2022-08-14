95a64026983d74ab0af9b9eda3162ffcc8099d44
Howard <howardchen1617@gmail.com>
2022-08-14 12:22:00 +0800

    [update] crawler1.js

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,20 +1,29 @@
 const { default: axios } = require('axios');
-const moment = require('moment');
+// const moment = require('moment');
 const fs = require('fs/promises');
 
 let stockArr = [];
-let queryDate = moment().format('YYYYMMDD');
+// let queryDate = moment().format('YYYYMMDD');
 let stockInfo = {};
 
 (async () => {
   let stockNo = await fs.readFile('stock.txt', 'utf-8');
   stockArr = stockNo.split(', ');
+
   for (const item of stockArr) {
-    let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${queryDate}&stockNo=${item}`);
-    stockInfo[item] = response.data;
+    try {
+      let response = await axios.get(`https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_${item}.tw&json=1&delay=0`);
+      stockInfo[item] = response.data;
+    } catch (e) {
+      console.error(e);
+    }
+
     console.log(stockInfo[item]);
-    await wait(3000);
+    await wait(2000);
   }
+
+  let fileInfo = JSON.stringify(stockInfo);
+  fs.writeFile('stockInfo.json', fileInfo);
 })();
 
 // 延遲請求用
