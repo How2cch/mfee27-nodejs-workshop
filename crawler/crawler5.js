9b1de7b65fe42c12a462badff52db97f5e1883b9
howard.chen <howardchen1617@gmail.com>
2022-08-21 02:07:05 +0800

    [update] crawler5.js

--- crawler/crawler5.js
+++ crawler/crawler5.js
@@ -2,38 +2,30 @@ const { default: axios } = require('axios');
 const moment = require('moment');
 const fs = require('fs/promises');
 const mysql = require('mysql2');
-require('dotenv');
+require('dotenv').config();
 
 let stockArr = [];
 let queryDate = moment().format('YYYYMMDD');
 
 (async () => {
-  let stockNo = await fs.readFile('stock.txt', 'utf-8');
+  let stockNo = await fs.readFile('./stock.txt', 'utf-8');
   stockArr = stockNo.split(', ');
 
   let connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'stock_mfee27',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
-  let a = 0;
-  for (const item of stockArr) {
-    console.log(`a`, a);
-    a++;
+
+  for (const stockItem of stockArr) {
     try {
-      let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
-        params: {
-          response: 'json',
-          date: queryDate,
-          stockNo: item,
-        },
-      });
+      // todo: 獲取 StockName ================================
 
       let queryNameResponse = await axios.get('https://www.twse.com.tw/zh/api/codeQuery', {
         params: {
-          query: item,
+          query: stockItem,
         },
       });
 
@@ -45,8 +37,32 @@ let queryDate = moment().format('YYYYMMDD');
       }
       let stockName = suggestion.split('\t').pop();
 
-      let saveNameResult = connection.execute('INSERT IGNORE INTO stocks (id, name) VALUES (?, ?)', [item, stockName]);
-      console.log(`saveNameResult`, saveNameResult);
+      connection.execute('INSERT IGNORE INTO stocks (id, name) VALUES (?, ?)', [stockItem, stockName]);
+      // console.log(`saveNameResult`, saveNameResult);
+
+      // todo: 獲取 StockData ================================
+
+      let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
+        params: {
+          response: 'json',
+          date: queryDate,
+          stockNo: stockItem,
+        },
+      });
+
+      let data = response.data.data.map((d) => {
+        d = d.map((value) => {
+          return value.replace(/,/g, '');
+        });
+        d[0] = parseInt(d[0].replace(/\//g, ''), 10) + 19110000;
+        d.unshift(stockItem);
+        return d;
+      });
+
+      let savePriceResult = await connection
+        .promise()
+        .query('INSERT IGNORE INTO stock_prices (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) VALUES ?', [data]);
+      console.log(savePriceResult);
     } catch (e) {
       console.error(e);
     }
