d0f17e482dadd9333713339d9d16b25eb1fc4be2
howard.chen <howardchen1617@gmail.com>
2022-08-21 16:33:54 +0800

    [create project] try to get API response

---
+++ stock-frontend/src/components/Stock.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Stock = () => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      let response = await axios.get('http://localhost:3001/api/1.0/stock');
+      setData(response.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
+      {data.map((stock) => {
+        return (
+          <div key={stock.id} className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/${stock.id}`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Stock;
