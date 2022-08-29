5f05ca72412d3304942520c346db719a7c8c0def
howard.chen <howardchen1617@gmail.com>
2022-08-29 11:42:47 +0800

    [update] add input handler

--- stock-frontend/src/components/Register.js
+++ stock-frontend/src/components/Register.js
@@ -1,24 +1,74 @@
+import React, { useState } from 'react';
+
 const Register = () => {
+  const [user, setUser] = useState({
+    email: '',
+    name: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleEdit = ({ target }) => {
+    let newData = { ...user };
+    newData[target.name] = target.value;
+    setUser(newData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className="bg-purple-100 h-screen md:h-full md:my-20 md:mx-16 lg:mx-28 xl:mx-40 py-16 md:py-8 px-24 text-gray-800 md:shadow md:rounded flex flex-col md:justify-center">
+    <form
+      className="bg-purple-100 h-screen md:h-full md:my-20 md:mx-16 lg:mx-28 xl:mx-40 py-16 md:py-8 px-24 text-gray-800 md:shadow md:rounded flex flex-col md:justify-center"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
       <h2 className="flex justify-center text-3xl mb-6 border-b-2 pb-2 border-gray-300">註冊帳戶</h2>
       <div className="mb-4 text-2xl">
         <label htmlFor="name" className="flex mb-2 w-32">
           Email
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="text" id="email" name="email" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="text"
+          id="email"
+          name="email"
+          value={user.email}
+          onChange={(e) => {
+            handleEdit(e);
+          }}
+        />
       </div>
       <div className="mb-4 text-2xl">
         <label htmlFor="name" className="flex mb-2 w-32">
           姓名
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="text" id="name" name="name" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="text"
+          id="name"
+          name="name"
+          value={user.name}
+          onChange={(e) => {
+            handleEdit(e);
+          }}
+        />
       </div>
       <div className="mb-4 text-2xl">
         <label htmlFor="password" className="flex mb-2 w-16">
           密碼
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="password" id="password" name="password" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="password"
+          id="password"
+          name="password"
+          value={user.password}
+          onChange={(e) => {
+            handleEdit(e);
+          }}
+        />
       </div>
       <div className="mb-8 text-2xl">
         <label htmlFor="password" className="flex mb-2 w-32">
@@ -29,6 +79,10 @@ const Register = () => {
           type="password"
           id="confirmPassword"
           name="confirmPassword"
+          value={user.confirmPassword}
+          onChange={(e) => {
+            handleEdit(e);
+          }}
         />
       </div>
       <div className="mb-8 text-2xl">
